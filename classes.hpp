ceb968725dc0ec6cd140ca5c4ae0ed62f23d7b87
iTzFabregas <fabricio-sampaio@usp.br>
2022-12-11 16:17:39 -0300

    add comments

--- classes.hpp
+++ classes.hpp
@@ -10,11 +10,20 @@
 
 using namespace std;
 
+/**
+ * @brief Stryct da aresta, dada pelo destinatpario da ligação e o seu peso (velociade)
+ * 
+ */
 typedef struct {
     int idPoPsConectado;
     int velocidade;
 } Edge;
 
+
+/**
+ * @brief Struct do vertice que tem as informações de cada ponto e a sua lista de adjacencia
+ * 
+ */
 typedef struct {
     int idConecta;
     string nomePoPs;
@@ -24,6 +33,11 @@ typedef struct {
     list<Edge> lista_adj;
 } Vertex; 
 
+
+/**
+ * @brief Classe do grafo, com as funções necessárias do grafo e o map dos vértices
+ * 
+ */
 class Graph {
 private:
     int num_vert;
@@ -31,19 +45,35 @@ private:
 
 public:
 
+    /**
+     * @brief Construtor do grafo que apenas seta o numero de vértices para 0
+     * 
+     */
     Graph() {
         this->num_vert = 0;
     }
 
+    /**
+     * @brief 
+     * 
+     * @param vertex1 
+     * @param aresta1 
+     */
     void insert_edge(Vertex, Edge);
 
-    /*bool exist_edge() {
-        return true;
-    }
-    */
-
+    /**
+     * @brief 
+     * 
+     */
     void print_graph();
 
+    /**
+     * @brief 
+     * 
+     * @param orig 
+     * @param dest 
+     * @return int 
+     */
     int dijkstra(int orig, int dest);
 };
 
