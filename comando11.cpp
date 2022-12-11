9aba06558514dd18e70b16edf93f2eb6b66d967c
iTzFabregas <fabricio-sampaio@usp.br>
2022-12-11 17:16:06 -0300

    14 working

--- comando11.cpp
+++ comando11.cpp
@@ -68,5 +68,33 @@ void command13(char* file_name) {
         
         
     }
+}
 
+void command14(char* file_name) {
+    Graph graph = command11(file_name);
+    int n;
+    cin >> n;
+
+    int origin;
+    int stop;
+    int destination;
+
+    for (size_t i = 0; i < n; i++) {
+        cin >> origin;
+        cin >> destination;
+        cin >> stop;
+
+        int flux1 = graph.dijkstra(origin, stop);
+        int flux2 = graph.dijkstra(stop, destination);
+
+        if (flux1 == -1 || flux2 == -1) {
+            cout << "Comprimento do caminho entre " << origin << " e " 
+            << destination << " parando em " << stop << ": -1" << endl;
+        } else {
+            cout << "Comprimento do caminho entre " << origin << " e " 
+            << destination << " parando em " << stop << ": " << flux1+flux2
+            << "Mbps" << endl;
+        }
+    }
+    
 }
